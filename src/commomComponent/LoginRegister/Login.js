44e52f526010dd388bf87745d8606cd31a8c42a4
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-03-21 17:51:59 +0900

    add : form login function

--- src/commomComponent/LoginRegister/Login.js
+++ src/commomComponent/LoginRegister/Login.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Login.scss";
 
 function Login() {
+  const [isEmail, setIsEmail] = useState("");
+  const [isPassword, setIsPassword] = useState("");
+
+  const onChange = (event) => {
+    const {
+      target: {name, value},
+    } = event;
+    if (name === "email") {
+      setIsEmail(value);
+    } else if (name === "password") {
+      setIsPassword(value);
+    }
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="login-wrapper">
       <div className="login-title">Memoring</div>
-      <input type="text" placeholder="Id" className="login-id" />
-      <input
-        type="password"
-        placeholder="Password"
-        className="login-password"
-      />
-      <button className="login-button">Login</button>
+      <form className="login-form" onSubmit={onSubmit}>
+        <input type="email" placeholder="Id" className="login-id" />
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-password"
+        />
+        <input type="submit" className="login-button" />
+      </form>
     </div>
   );
 }
