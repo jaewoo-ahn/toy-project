d85d8e6cb7f7615e49e56a0d3edaececd802074a
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2022-03-28 17:23:11 +0900

    add : firebase config

---
+++ src/FireBase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import {initializeApp} from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFslTmD96r_z0eAnsJw3m65fzvOaEasmg",
+  authDomain: "memoring-toy.firebaseapp.com",
+  projectId: "memoring-toy",
+  storageBucket: "memoring-toy.appspot.com",
+  messagingSenderId: "611031949254",
+  appId: "1:611031949254:web:28e989d18210b1c1435cc7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
